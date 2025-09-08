4acc92e69d7ab855f0d76c8963fdb7a907e95cd0
AuriRL <165871123+AuriRL@users.noreply.github.com>
2025-09-08 15:38:36 +1000

    Assignment 1 commit

--- Assignment 1/Assignment 1/grok/synchroniser.v
+++
@@ -1,9 +0,0 @@
-// Unchanged modules
-module synchroniser (input clk, x, output y);
-    reg x_q0, x_q1;
-    always @(posedge clk) begin
-        x_q0 <= x;
-        x_q1 <= x_q0;
-    end
-    assign y = x_q1;
-endmodule
